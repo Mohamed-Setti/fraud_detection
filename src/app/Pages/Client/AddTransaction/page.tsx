984f73c4a2a106cccac172c56c1ce19f265392ac
fatma garali <fatmagarali46@gmail.com>
2025-11-20 09:08:59 +0100

    Refactor router import to use 'next/router' for consistency

--- src/app/Pages/Client/AddTransaction/page.tsx
+++ src/app/Pages/Client/AddTransaction/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import useRouter  from "next/router";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 
